31644d265f7951bc22d73b1b741a2fc9a5413a37
hasnain saeed <ihasnain4gmail.com>
2024-08-01 21:07:32 +0500

    fix minor changes

--- server/api/exercise/[id].ts
+++ server/api/exercise/[id].ts
@@ -1,10 +1,9 @@
 import { defineEventHandler, getRouterParam, H3Event } from 'h3'
 import { readFileSync } from 'fs'
 import { join } from 'path'
-
 export default defineEventHandler((event: H3Event) => {
   const id = getRouterParam(event, 'id')
-  const filePath = join(process.cwd(), '/public/data/exercises.json')
+  const filePath = join(process.cwd(), 'public/data', 'exercises.json');
   const data = JSON.parse(readFileSync(filePath, 'utf8'))
   const exercise = data.find((item: any) => item.id === id)
   if (exercise) {
