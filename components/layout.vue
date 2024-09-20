79434b96fd63767073e1e53d604c194b7bee5aba
hasnain saeed <ihasnain4@gmail.com>
2024-09-21 01:46:14 +0500

    fix: object bottom issues

--- components/layout.vue
+++ components/layout.vue
@@ -1,22 +1,28 @@
 <template>
-    <main>
-        <div class="flex overflow-hidden">
-            <div class="z-0 sticky h-screen w-1/3 md:block hidden shrink-0">
-                <div class="block overflow-hidden relative h-full">
-                    <div class="z-10 relative mt-16 px-5 space-y-5">
-                        <slot name="header"></slot>
-                    </div>
-                    <img src="/images/auth-bg.png" alt="blue background image"
-                        class="block w-full h-full absolute inset-0 ">
-                    <img src="/images/man-women.png" alt="man and women image"
-                        class="absolute left-0 bottom-0 box-border p-0 border-none m-0 block object-contain w-full h-1/2">
-                </div>
-            </div>
-            <div class="overflow-auto h-screen flex-grow z-10">
-                <div class="w-full px-6 mb-6 mt-16">
-                        <slot name="body"></slot>
-                </div>
-            </div>
+  <main>
+    <div class="flex overflow-hidden">
+      <div class="z-0 sticky h-screen w-1/3 md:block hidden shrink-0">
+        <div class="block overflow-hidden relative h-full">
+          <div class="z-10 relative mt-16 px-5 space-y-5">
+            <slot name="header"></slot>
+          </div>
+          <img
+            src="/images/auth-bg.png"
+            alt="blue background image"
+            class="block w-full h-full absolute inset-0"
+          />
+          <img
+            src="/images/man-women.png"
+            alt="man and women image"
+            class="absolute left-0 bottom-0 box-border p-0 border-none m-0 block object-contain object-bottom w-full h-1/2"
+          />
         </div>
-    </main>
+      </div>
+      <div class="overflow-auto h-screen flex-grow z-10">
+        <div class="w-full px-6 mb-6 mt-16">
+          <slot name="body"></slot>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
