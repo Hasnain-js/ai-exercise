773fa29f26c0a4d6f4e5bbed9b0bde3c29d103d3
hasnain saeed <ihasnain4@gmail.com>
2024-09-21 01:52:17 +0500

    fix: minor changes

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -48,7 +48,7 @@ const onSubmit = () => {
         useUserStore().data = res.data.user;
         useUserStore().token = res.data.token;
         useUserStore().isAuthenticated = true;
-        useRouter().push(`/exercise/${Number(res.data.user.step) + 1 || "1"}`);
+        useRouter().push(`/exercise/${Number(res.data.user.step) || "1"}`);
       })
       .catch((error) => {
         const message = error.response?.data?.message;
